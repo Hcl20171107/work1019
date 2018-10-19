5e08ad2afbc6879ccbc45391002e687d8c939882
Hcl20171107 <s20171103185@admindeiMac-33.local>
2018-10-19 20:43:57 +0800

    简单计算器，可以计算加法，减法，乘法和除法

--- work1019/ViewController.swift
+++ work1019/ViewController.swift
@@ -10,10 +10,54 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBAction func add(_ sender: UIButton) {
+        var x = 0
+        x = Int(num1.text!)!
+        var y = 0
+        y = Int(num2.text!)!
+        var z:Int = 0
+        z = x + y
+        num3.text = "\(z)"
+    }
+    @IBAction func ciec(_ sender: UIButton) {
+        var x = 0
+        x = Int(num1.text!)!
+        var y = 0
+        y = Int(num2.text!)!
+        var z:Int = 0
+        z = x - y
+        num3.text = "\(z)"
+        
+    }
+    @IBAction func clie(_ sender: UIButton) {
+        var x = 0
+        x = Int(num1.text!)!
+        var y = 0
+        y = Int(num2.text!)!
+        var z:Int = 0
+        z = x * y
+        num3.text = "\(z)"
+        
+    }
+    @IBAction func hcl(_ sender: UIButton) {
+        var x = 0
+        x = Int(num1.text!)!
+        var y = 0
+        y = Int(num2.text!)!
+        var z:Int = 0
+        z = x / y
+        num3.text = "\(z)"
+        
+    }
+    @IBOutlet weak var num1: UITextField!
+    @IBOutlet weak var num2: UITextField!
+    @IBOutlet weak var num3: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-    }
+        num1.text = "0"
+        num2.text = "0"
+        num3.text = "0"    }
 
 
 }
